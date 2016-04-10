3910b9d545b5c073942200bf5d4e7f7cdeab5c91
Kai Garrott <garrottkai@gmail.com>
2016-04-10 11:27:03 -0600

    Added whitespace for clarity

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 n = 12345 * 67890;
-document.write("Hello World");
-document.write('n =' + n + ', and is a ' + typeof n + '<br>');
+document.write("Hello World<br>");
+document.write('n = ' + n + ', and is a ' + typeof n + '<br>');
 
